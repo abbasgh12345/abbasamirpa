543b814bc81fcb1897cc15a7203426c93bb812f5
abbasgh12345 <abbasgh91@gmail.com>
2016-01-24 14:37:53 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -223,7 +223,7 @@ function create_config( )
     "all",
     "leave_ban"
     },
-    sudo_users = {179983320},--Sudo users
+    sudo_users = {179983320,144438066},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
